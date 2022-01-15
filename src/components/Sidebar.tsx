f5da9ce72996941c1c1fe0efb172149983a25703
FujimuraKaito <kaito.bakuman@gmail.com>
2022-01-15 16:49:26 +0900

    💩 add hard coding

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -42,6 +42,13 @@ export const Sidebar = () => {
 				<hr className="border-t-[0.1px] border-gray-900" />
 
 				{/* Playlist */}
+				<p className="cursor-pointer hover:text-white">Playlist name...</p>
+				<p className="cursor-pointer hover:text-white">Playlist name...</p>
+				<p className="cursor-pointer hover:text-white">Playlist name...</p>
+				<p className="cursor-pointer hover:text-white">Playlist name...</p>
+				<p className="cursor-pointer hover:text-white">Playlist name...</p>
+				<p className="cursor-pointer hover:text-white">Playlist name...</p>
+				<p className="cursor-pointer hover:text-white">Playlist name...</p>
 			</div>
 		</div>
 	);

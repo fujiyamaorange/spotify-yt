d363f8ddfc22fa2cb9776def556bff7728e2a163
FujimuraKaito <kaito.bakuman@gmail.com>
2022-01-15 16:49:02 +0900

    ♻️ refactor index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,8 @@ import { Sidebar } from "../components/Sidebar";
 
 export default function Home() {
 	return (
-		<div className="">
-			<h1>This is a DOPE spotify 2.0 build</h1>
-
-			<main>
+		<div className="h-screen overflow-hidden bg-black">
+			<main className="">
 				<Sidebar />
 				{/* Center */}
 

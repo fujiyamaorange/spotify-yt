1c6d666206af90795dffdb9023291d6e19add98b
FujimuraKaito <kaito.bakuman@gmail.com>
2022-01-16 13:27:21 +0900

    ✨ add middleware

---
+++ src/pages/_middleware.ts
@@ -0,0 +1,33 @@
+import { getToken } from "next-auth/jwt";
+import { NextResponse, NextRequest } from "next/server";
+
+const JWT_SECRET = process.env.JWT_SECRET ?? "";
+
+export async function middleware(req: NextRequest) {
+	// Token will exist if user is logged in
+	const token = await getToken({ req, secret: JWT_SECRET });
+
+	const { pathname } = req.nextUrl;
+	// console.log("PATHNAME: ", pathname);
+	// console.log("TOKEN: ", token);
+
+	if (token && pathname === "/login") {
+		// ログインしている&login画面に行こうとしている
+		// →/にリダイレクトする
+		return NextResponse.redirect("/");
+	}
+
+	// Allow the requests if the following is true...
+	// 1. its a request for next-auth session & provider fetching
+	// 2. if the token exists
+	if (pathname.includes("/api/auth") || token) {
+		// そのまま返す
+		return NextResponse.next();
+	}
+
+	// redirect them to login page if they dont have token & are requesting a protected route
+	if (!token && pathname !== "/login") {
+		// ログインページに返す
+		return NextResponse.redirect("/login");
+	}
+}

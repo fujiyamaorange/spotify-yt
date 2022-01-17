691620aaa3b08fd2366cbb73f3a96b601b6b3ab8
FujimuraKaito <kaito.bakuman@gmail.com>
2022-01-17 21:53:33 +0900

    🎨 use Song component

--- src/components/Songs.tsx
+++ src/components/Songs.tsx
@@ -1,15 +1,17 @@
-import { useRecoilValue } from "recoil";
-import { playlistState } from "@/atoms/playlistAtom";
+import { useRecoilValue } from 'recoil'
+
+import { playlistState } from '@/atoms/playlistAtom'
+import { Song } from './Song'
 
 export const Songs: React.VFC = () => {
-	const playlist = useRecoilValue(playlistState);
+  const playlist = useRecoilValue(playlistState)
 
-	return (
-		<div className="text-white">
-			{Object.keys(playlist).length > 0 &&
-				playlist.tracks.items.map((track) => (
-					<div key={track.track.id}>{track.track.name}</div>
-				))}
-		</div>
-	);
-};
+  return (
+    <div className="flex flex-col px-8 py-4 space-y-1 text-white">
+      {Object.keys(playlist).length > 0 &&
+        playlist.tracks.items.map((track, i) => (
+          <Song key={track.track.id} track={track.track} order={i} />
+        ))}
+    </div>
+  )
+}

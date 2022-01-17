3600a1edefd32bae4b76a4f8bb2d1e7b288a6871
FujimuraKaito <kaito.bakuman@gmail.com>
2022-01-17 21:54:07 +0900

    🚨 lint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,30 @@
-import { Sidebar } from "../components/Sidebar";
-import { Center } from "@/components/Center";
-import { getSession } from "next-auth/react";
-import { GetServerSideProps } from "next";
+import { Sidebar } from '../components/Sidebar'
+import { Center } from '@/components/Center'
+import { getSession } from 'next-auth/react'
+import { GetServerSideProps } from 'next'
+import { Player } from '@/components/Player'
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-	const session = await getSession(context);
+  const session = await getSession(context)
 
-	return {
-		props: {
-			session,
-		},
-	};
-};
+  return {
+    props: {
+      session,
+    },
+  }
+}
 
 export default function Home() {
-	return (
-		<div className="h-screen overflow-hidden bg-black">
-			<main className="flex">
-				<Sidebar />
-				<Center />
+  return (
+    <div className="h-screen overflow-hidden bg-black">
+      <main className="flex">
+        <Sidebar />
+        <Center />
 
-				<div>{/* Player */}</div>
-			</main>
-		</div>
-	);
+        <div className="sticky bottom-0">
+          <Player />
+        </div>
+      </main>
+    </div>
+  )
 }

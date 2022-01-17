95148227113bc4b857e6ed819bee4ef5f769b696
FujimuraKaito <kaito.bakuman@gmail.com>
2022-01-17 20:57:42 +0900

    🚨 lint

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,86 +1,80 @@
 import {
-	HomeIcon,
-	SearchIcon,
-	LibraryIcon,
-	PlusCircleIcon,
-	HeartIcon,
-	RssIcon,
-} from "@heroicons/react/outline";
-import { signOut, useSession } from "next-auth/react";
-import { useState, useEffect } from "react";
+  HomeIcon,
+  SearchIcon,
+  LibraryIcon,
+  PlusCircleIcon,
+  HeartIcon,
+  RssIcon,
+} from '@heroicons/react/outline'
+import { signOut, useSession } from 'next-auth/react'
+import { useState, useEffect } from 'react'
 
-import { useSpotify } from "@/hooks/useSpotify";
-import { useRecoilState } from "recoil";
-import { playlistIdState } from "@/atoms/playlistAtom";
+import { useSpotify } from '@/hooks/useSpotify'
+import { useRecoilState } from 'recoil'
+import { playlistIdState } from '@/atoms/playlistAtom'
 
 export const Sidebar = () => {
-	const spotifyApi = useSpotify();
-	const { data: session, status } = useSession();
-	const [playlists, setPlaylists] = useState<
-		SpotifyApi.PlaylistObjectSimplified[]
-	>([]);
-	const [playlistId, setPlaylistId] = useRecoilState(playlistIdState);
+  const spotifyApi = useSpotify()
+  const { data: session, status } = useSession()
+  const [playlists, setPlaylists] = useState<
+    SpotifyApi.PlaylistObjectSimplified[]
+  >([])
+  const [playlistId, setPlaylistId] = useRecoilState(playlistIdState)
 
-	useEffect(() => {
-		if (spotifyApi.getAccessToken()) {
-			spotifyApi.getUserPlaylists().then((data) => {
-				setPlaylists(data.body.items);
-			});
-		}
-	}, [session, spotifyApi]);
+  useEffect(() => {
+    if (spotifyApi.getAccessToken()) {
+      spotifyApi.getUserPlaylists().then((data) => {
+        setPlaylists(data.body.items)
+      })
+    }
+  }, [session, spotifyApi])
 
-	return (
-		<div className="h-screen p-5 overflow-y-scroll text-xs text-gray-500 border-r border-gray-900 scrollbar-hide lg:text-sm sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex">
-			<div className="space-y-4">
-				<button
-					className="flex items-center space-x-2 transition duration-150 hover:text-white"
-					onClick={() => signOut()}
-				>
-					<p>Logout</p>
-				</button>
-				<button className="flex items-center space-x-2 transition duration-150 hover:text-white">
-					<HomeIcon className="w-5 h-5" />
-					<p>Home</p>
-				</button>
-				<button className="flex items-center space-x-2 transition duration-150 hover:text-white">
-					<SearchIcon className="w-5 h-5" />
-					<p>Search</p>
-				</button>
-				<button className="flex items-center space-x-2 transition duration-150 hover:text-white">
-					<LibraryIcon className="w-5 h-5" />
-					<p>Your Library</p>
-				</button>
+  return (
+    <div className="h-screen p-5 overflow-y-scroll text-xs text-gray-500 border-r border-gray-900 scrollbar-hide lg:text-sm sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex">
+      <div className="space-y-4">
+        <button className="flex items-center space-x-2 transition duration-150 hover:text-white">
+          <HomeIcon className="w-5 h-5" />
+          <p>Home</p>
+        </button>
+        <button className="flex items-center space-x-2 transition duration-150 hover:text-white">
+          <SearchIcon className="w-5 h-5" />
+          <p>Search</p>
+        </button>
+        <button className="flex items-center space-x-2 transition duration-150 hover:text-white">
+          <LibraryIcon className="w-5 h-5" />
+          <p>Your Library</p>
+        </button>
 
-				<hr className="border-t-[0.1px] border-gray-900" />
+        <hr className="border-t-[0.1px] border-gray-900" />
 
-				<button className="flex items-center space-x-2 transition duration-150 hover:text-white">
-					<PlusCircleIcon className="w-5 h-5" />
-					<p>Create Playlist</p>
-				</button>
-				<button className="flex items-center space-x-2 transition duration-150 hover:text-white">
-					<HeartIcon className="w-5 h-5" />
-					<p>Liked Songs</p>
-				</button>
-				<button className="flex items-center space-x-2 transition duration-150 hover:text-white">
-					<RssIcon className="w-5 h-5" />
-					<p>Your episodes</p>
-				</button>
+        <button className="flex items-center space-x-2 transition duration-150 hover:text-white">
+          <PlusCircleIcon className="w-5 h-5" />
+          <p>Create Playlist</p>
+        </button>
+        <button className="flex items-center space-x-2 transition duration-150 hover:text-white">
+          <HeartIcon className="w-5 h-5" />
+          <p>Liked Songs</p>
+        </button>
+        <button className="flex items-center space-x-2 transition duration-150 hover:text-white">
+          <RssIcon className="w-5 h-5" />
+          <p>Your episodes</p>
+        </button>
 
-				<hr className="border-t-[0.1px] border-gray-900" />
+        <hr className="border-t-[0.1px] border-gray-900" />
 
-				{/* Playlist→now I use New Release */}
-				{playlists.map((playlist) => {
-					return (
-						<p
-							key={playlist.id}
-							className="transition duration-150 cursor-pointer hover:text-white"
-							onClick={() => setPlaylistId(playlist.id)}
-						>
-							{playlist.name}
-						</p>
-					);
-				})}
-			</div>
-		</div>
-	);
-};
+        {/* Playlist→now I use New Release */}
+        {playlists.map((playlist) => {
+          return (
+            <p
+              key={playlist.id}
+              className="transition duration-150 cursor-pointer hover:text-white"
+              onClick={() => setPlaylistId(playlist.id)}
+            >
+              {playlist.name}
+            </p>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
